53ce76b0f21d10c1479e44e6494ed3d94d4ecca1
Pablo <148821043+pablobmm@users.noreply.github.com>
2025-11-12 15:29:32 -0300

    feat: implementa as classes audio,musica e podcast

---
+++ src/br/com/alura/minhasmusicas/modelos/Musica.java
@@ -0,0 +1,31 @@
+package br.com.alura.minhasmusicas.modelos;
+
+public class Musica {
+    private String audio;
+    private String cantor;
+    private String genero;
+
+    public String getAudio() {
+        return audio;
+    }
+
+    public void setAudio(String audio) {
+        this.audio = audio;
+    }
+
+    public String getCantor() {
+        return cantor;
+    }
+
+    public void setCantor(String cantor) {
+        this.cantor = cantor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
